b24b2f167f6603da6187d004e7ecc9449bf60d3d
rokasbeprotis <167200134+rokasbeprotis@users.noreply.github.com>
2024-08-06 15:42:56 +0300

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
